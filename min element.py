1350ced928d65248bda6f7c02a648798a8c53198
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:30:57 +0530

    Add files via upload

---
+++ min element.py
@@ -0,0 +1,7 @@
+#Write a program to input three numbers(A, B & C) from the user and print the minimum element among A, B & C. 
+
+A = float(input("Enter first number (A): "))
+B = float(input("Enter second number (B): "))
+C = float(input("Enter third number (C): "))
+print("Minimum:", min(A, B, C))
+
