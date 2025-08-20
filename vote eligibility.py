e9d100b7da93ebba9fac25a2e213d24131a7c159
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:24:46 +0530

    Add files via upload

---
+++ vote eligibility.py
@@ -0,0 +1,9 @@
+
+#WAP to check whether a person is eligible to vote or not.
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
