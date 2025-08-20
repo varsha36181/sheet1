2d872a0afb6910ac1f794700c05cfe4eb5552564
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:35:54 +0530

    Add files via upload

---
+++ average 5.py
@@ -0,0 +1,11 @@
+#Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers. 
+
+A = float(input("Enter A: "))
+B = float(input("Enter B: "))
+C = float(input("Enter C: "))
+D = float(input("Enter D: "))
+E = float(input("Enter E: "))
+
+average = (A + B + C + D + E) / 5
+
+print("Average:", average)
