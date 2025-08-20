8715855899d99a4a2b7a3b6795fa94cf7ac04edc
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:34:40 +0530

    Add files via upload

---
+++ divide7,digit 5.py
@@ -0,0 +1,7 @@
+
+num = int(input("Enter a number: "))
+
+if num % 7 == 0 and num %510 == 4:
+    print(num," is divisible by 7 and ends with 5")
+else:
+    print(num, "here,we got to use it againn.")
