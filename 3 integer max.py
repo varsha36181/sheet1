ea7f22e1abb47ab7f4181ead62d91dc08b5e0d63
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:36:17 +0530

    Add files via upload

---
+++ 3 integer max.py
@@ -0,0 +1,9 @@
+#Read three integers and print their maximum.    
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = int(input("Enter third integer: "))
+
+maximum = max(a, b, c)
+
+print("The maximum number is:", maximum)
