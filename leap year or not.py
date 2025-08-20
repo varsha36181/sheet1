eb35c72715f92dfff2ca82627b75000e038879fc
varsha <varsha.gla_bca24@gla.ac.in>
2025-08-20 18:32:02 +0530

    Add files via upload

---
+++ leap year or not.py
@@ -0,0 +1,8 @@
+#WAP to check whether a year is a leap year or not.
+
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
